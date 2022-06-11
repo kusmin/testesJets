be26f76605c8ac62979ebab2e737bb498814633d
Renan Ribeiro <renan@itexto.com.br>
2022-06-11 17:25:03 -0300

    eslint

--- __test__/calcula-valor.test.js
+++ __test__/calcula-valor.test.js
@@ -1,1 +1,26 @@
 const calculaValor = require('../src/calcula-valor.js')
+
+describe('calcularMontante', () => {
+    test('Com uma prestação o montante é igual ao capital', () => {
+        //Operação
+        const montante = calculaValor.calcularMontante(100, 0.0175, 1)
+
+        // Resultado ou Comportamento esperado
+        expect(montante).toBe(100)
+    })
+
+    test('Com 4 prestações o montante é acrescido de juros', () => {
+        //Operação
+        const montante = calculaValor.calcularMontante(500, 0.025, 4)
+
+        // Resultado ou comportamento esperado
+        expect(montante).toBe(538.45)
+    })
+})
+
+describe('arredondar', () => {
+    test('Arredondar duas casas decimais', () => {
+        const resultado = calculaValor.arredondar(538.4453124999998)
+        expect(resultado).toBe(538.45)
+    })
+})
