01f192f8a0ee5439496f2ae9b858b047fe07d633
Renan Ribeiro <renan@itexto.com.br>
2022-06-12 19:54:35 -0300

    testes

--- __test__/calcula-valor.test.js
+++ __test__/calcula-valor.test.js
@@ -1,26 +1,76 @@
 const calculaValor = require('../src/calcula-valor.js')
 
 describe('calcularMontante', () => {
-    test('Com uma prestação o montante é igual ao capital', () => {
-        //Operação
-        const montante = calculaValor.calcularMontante(100, 0.0175, 1)
+  test('Com uma prestação o montante é igual ao capital', () => {
+    // Operação
+    const montante = calculaValor.calcularMontante(100, 0.0175, 1)
 
-        // Resultado ou Comportamento esperado
-        expect(montante).toBe(100)
-    })
+    // Resultado ou Comportamento esperado
+    expect(montante).toBe(100)
+  })
 
-    test('Com 4 prestações o montante é acrescido de juros', () => {
-        //Operação
-        const montante = calculaValor.calcularMontante(500, 0.025, 4)
+  test('Com 4 prestações o montante é acrescido de juros', () => {
+    // Operação
+    const montante = calculaValor.calcularMontante(500, 0.025, 4)
 
-        // Resultado ou comportamento esperado
-        expect(montante).toBe(538.45)
-    })
+    // Resultado ou comportamento esperado
+    expect(montante).toBe(538.45)
+  })
 })
 
 describe('arredondar', () => {
-    test('Arredondar duas casas decimais', () => {
-        const resultado = calculaValor.arredondar(538.4453124999998)
-        expect(resultado).toBe(538.45)
-    })
+  test('Arredondar duas casas decimais', () => {
+    const resultado = calculaValor.arredondar(538.4453124999998)
+    expect(resultado).toBe(538.45)
+  })
+
+  test('1.005 deve retornar 1.01', () => {
+    const resultado = calculaValor.arredondar(1.005)
+
+    expect(resultado).toBe(1.01)
+  })
+})
+
+describe('calcular prestações', () => {
+  test('O numero de parcelas é igual ao numero de prestações', () => {
+    const numeroPrestacoes = 6
+    const prestacoes = calculaValor.calcularPrestacoes(200, numeroPrestacoes)
+
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+  })
+
+  test('Uma unica prestação, valor vai ser igual ao montante', () => {
+    const numeroPrestacoes = 1
+
+    const prestacoes = calculaValor.calcularPrestacoes(50, numeroPrestacoes)
+
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+    expect(prestacoes[0]).toBe(50)
+  })
+
+  test('Duas prestacoes, valor igual à metade do valor do montante', () => {
+    const numeroPrestacoes = 2
+    const prestacoes = calculaValor.calcularPrestacoes(50, numeroPrestacoes)
+
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+    expect(prestacoes[0] + prestacoes[1]).toBe(50)
+    expect(prestacoes[0]).toBe(50 / 2)
+    expect(prestacoes[0]).toBe(prestacoes[1])
+  })
+
+  test('Valor da soma das prestações deve ser igual ao montante com duas casas decimais', () => {
+    const numeroPrestacoes = 3
+    const montante = 100
+
+    const prestacoes = calculaValor.calcularPrestacoes(montante, numeroPrestacoes)
+
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+    const soma = calculaValor.arredondar(prestacoes[0] + prestacoes[1] + prestacoes[2])
+    expect(soma).toBe(montante)
+
+    for (let i = 0; i < prestacoes.length - 1; i++) {
+      const j = i + 1
+      expect(prestacoes[i]).toBeGreaterThanOrEqual(prestacoes[j])
+    }
+  })
 })
--- src/calcula-valor.js
+++ src/calcula-valor.js
@@ -1,15 +1,32 @@
-function calcularMontante(capital, taxa, periodo) {
-    let montante = capital * Math.pow((1 + taxa), periodo - 1);
-    montante = arredondar(montante)
-    return montante;
+function calcularMontante (capital, taxa, periodo) {
+  let montante = capital * Math.pow((1 + taxa), periodo - 1)
+  montante = arredondar(montante)
+  return montante
 }
 
-function arredondar(valor) {
-    const precisao = 100
-    const arredondado = Math.round(valor * precisao) / precisao;
-    return arredondado;
+function arredondar (valor) {
+  const precisao = 100
+  return Math.round((valor + Number.EPSILON) * precisao) / precisao
 }
 
-module.exports = {
-    calcularMontante, arredondar
+function calcularPrestacoes (montante, numeroParcelas) {
+  const prestacaoBase = arredondar(montante / numeroParcelas)
+  const array = Array(numeroParcelas).fill(prestacaoBase)
+
+  let resto = arredondar((montante - (prestacaoBase * numeroParcelas)))
+
+  let index = 0
+  while (resto) {
+    array[index] = arredondar(array[index] + 0.01)
+    resto = arredondar(resto - 0.01)
+    index++
+  }
+
+  for (const prestacao of array) {
+    console.log(prestacao)
+  }
+
+  return array
 }
+
+module.exports = { calcularMontante, arredondar, calcularPrestacoes }
